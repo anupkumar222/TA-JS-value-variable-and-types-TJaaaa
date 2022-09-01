349e0427f5d09eedd26634833806414803bf2e57
anup kumar <kanup414@gmail.com>
2022-09-01 13:03:25 +0530

    Assignment: Level 3

--- block-BJaabg/code/index.js
+++ block-BJaabg/code/index.js
@@ -15,4 +15,21 @@ var bank_balance = 303.91;
 var amount = 0;
 // your code goes here
 
+while(bank_balance > amount){
+    amount += PHONE_PRICE;
+    if( amount < SPENDING_THRESHOLD){
+        amount += ACCESSORY_PRICE
+    }
+  }
+  let tax = amount * TAX_RATE;
+
+  let taxedAmount = amount + tax;
+  console.log(`${taxedAmount}`);
+  if(taxedAmount < bank_balance) {
+    console.log("you can afford it")
+  }
+  else {
+    console.log(`you can't afford buying the phone`)
+  }
 // ⛑ Answer of the above will `$334.76`.
+
